43aa2931774794d27e1458d07ca54259f0b739a0
Tomv <tom.v@walkme.com>
2018-08-05 14:06:15 +0300

    new event page

--- client/newFundEvent/newFundEvent.js
+++ client/newFundEvent/newFundEvent.js
@@ -24,13 +24,76 @@ export default class FundEvent extends Component {
         console.log(window.editor.getData());
     }
 
+    handleImageChange(e) {
+        e.preventDefault();
+
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            this.setState({
+                file: file
+            });
+        };
+
+        reader.readAsDataURL(file)
+    }
+
     render() {
         return (
             <div>
                 <form className="create-campaign" onSubmit={this.create.bind(this)}>
-                    <input type="text" ref="name" placeholder="name"/>
+                    <label>שם העמותה</label>
+                    <input type="text" ref="name"/>
+                    <br/>
+
+                    <label>לוגו העמותה</label>
+                    <input className="fileInput" type="file" onChange={(e)=>this.handleImageChange(e)} />
+                    <br/>
+
+                    <label>דף הבית</label>
+                    <input type="text" ref="homePage"/>
+                    <br/>
+
+                    <label>מספר עמותה</label>
+                    <input type="text" ref="associationId"/>
+                    <br/>
+
+                    <label>טלפון</label>
+                    <input type="text" ref="phone"/>
+                    <br/>
+
+                    <label>כתבו משהו...</label>
                     <textarea name="content" id="editor"></textarea>
-                    <input type="button" value="Print data in console" onClick={this.getEditorData.bind(this)}/>
+                    <br/>
+
+                    <label>הזמן פרטי תשלום</label>
+                    <br/>
+                    <label>בנק</label>
+                    <input type="text" ref="bankNumber"/>
+                    <br/>
+
+                    <label>מספר סניף</label>
+                    <input type="text" ref="bankBranch"/>
+                    <br/>
+
+                    <label>מספר חשבון</label>
+                    <input type="text" ref="bankAccount"/>
+                    <br/>
+
+                    <label>שם בעל החשבון</label>
+                    <input type="text" ref="bankAccountOwner"/>
+                    <br/>
+
+                    <label>שם משתמש ב-PayPal</label>
+                    <input type="text" ref="paypalEmail"/>
+                    <br/>
+
+                    <label>מספר טלפון (bit)</label>
+                    <input type="text" ref="bitPhone"/>
+                    <br/>
+
+                    <input type="submit" value="צור עמוד"/>
                 </form>
             </div>
         );
