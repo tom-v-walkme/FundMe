a34c20d8aa65625635136238941951c0ebe9ed19
Tomv <tom.v@walkme.com>
2018-08-05 19:13:05 +0300

    navigate to home page when clicking header

--- client/shared/fundEventHeader.js
+++ client/shared/fundEventHeader.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import {withRouter} from "react-router-dom";
 
 // Task component - represents a single todo item
-export default class FundEventHeader extends Component {
+class FundEventHeader extends Component {
+    goHome() {
+        this.props.history.push('/home');
+    }
+
     render() {
         return (
             <div className="fundEventHeader">
-                <img src={'/images/fundme.svg'} className="fundMeImageLogo" />
+                <img src={'/images/fundme.svg'} className="fundMeImageLogo" onClick={this.goHome.bind(this)}/>
             </div>
         );
     }
 }
+
+export default withRouter(FundEventHeader)
