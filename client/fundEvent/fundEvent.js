e75f0911f5c7768f85c072be660676d25d2cf758
Amir <amir.s@walkme.com>
2018-08-05 14:21:51 +0300

    issue : WM-   comment : reviewer :

--- client/fundEvent/fundEvent.js
+++ client/fundEvent/fundEvent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {FundEvents} from '../../imports/api/fundEvents';
 import {withTracker} from 'meteor/react-meteor-data';
 import FundEventHeader from "./fundEventHeader";
+import FundEventBar from "./fundEventBar";
+import FundEventImage from "./funcEventImage";
 
 class FundEvent extends Component {
     fundEvent;
@@ -14,6 +16,21 @@ class FundEvent extends Component {
         return (
             <div>
                 <FundEventHeader />
+                <br/>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <FundEventBar/>
+                            </td>
+                            <td>
+                                <FundEventImage/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br/>
+
                 <p>{this.props.fundEvent && this.props.fundEvent.name}</p>
             </div>
         );
